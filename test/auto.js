743bfb25b1e103f261c2379bb839910e9498ed9d
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-15 22:25:40 -0700

    basic test case + few more deps

--- test/auto.js
+++ test/auto.js
@@ -1,17 +1,27 @@
 'use strict';
 
 const async = require('../lib');
-const assert = require('assert');
+const should = require('should');
+const Promise = require('bluebird');
 
+describe('bluebird-auto', function() {
 
-describe('auto', function() {
-  it('basics', function() {
+  it('should return a promise', () => {
+    const auto = async.auto();
+    auto.should.be.a.Promise();
+  }); 
+
+  it('should call tasks based on their requirements', function() {
     let callOrder = [];
 
+    const tasks = {
+      getData: Promise.resolve(1),
+      makeFolder: Promise.resolve(1)
+    }
 
-    async.auto({})
-
-
+    async.auto(tasks).then(res => {
+      console.log(res)
+    });
   });
 
 });

4e5800aac62367e988861715ee5563f433f6b082
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-15 22:02:55 -0700

    boilerplate

---
+++ test/auto.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const async = require('../lib');
+const assert = require('assert');
+
+
+describe('auto', function() {
+  it('basics', function() {
+    let callOrder = [];
+
+
+    async.auto({})
+
+
+  });
+
+});

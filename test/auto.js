d559a4c56bfd423e0cb03b16dace326d10f844f0
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-24 23:01:22 -0700

    auto updated

--- test/auto.js
+++ test/auto.js
@@ -5,26 +5,8 @@ const should = require('should');
 const Promise = require('bluebird');
 
 describe('bluebird-auto', function() {
-
   it('should return a promise', () => {
     const auto = async.auto();
     auto.should.be.a.Promise();
-  });
-
-  it('should call tasks based on their requirements', function() {
-    let callOrder = [];
-
-    const tasks = {
-      readData: Promise.resolve(1),
-      showData: ['readData', results => {
-        // results.readData is the file's contents
-        // ...
-
-      }]
-    }
-
-    async.auto(tasks).then(res => {
-      console.log(res)
-    });
   });
 });
